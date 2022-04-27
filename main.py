a40f1aed98d450cec40c067fd2a9239e9b54f590
Felipe Cardenas Lima Namour <felipe@pop-os.localdomain>
2022-04-27 14:46:20 -0300

    salve

---
+++ main.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+'''
+Implemente o algoritmo descrito acima para a decomposi¸c˜ao LU de uma matriz tridiagonal A n × n.
+As matrizes A, L e U devem ser armazenadas em vetores conforme descrito no texto. Implemente
+tamb´em o algoritmo para a resolu¸c˜ao de um sistema linear tridiagonal usando a decomposi¸c˜ao LU da
+matriz. Fa¸ca as implementa¸c˜oes de forma que elas possam ser usadas como partes de outros programas.
+'''
+
+def montaMatriz(n):
+    A = []
+    for i in range(n):
+        for j in range(n):
+            
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
