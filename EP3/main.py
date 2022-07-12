f53c3093e0bc76e7af2a6713aed3ab3b0b79515d
Felipe <felipe@pop-os.localdomain>
2022-07-12 19:01:20 -0300

    4.3 funcionando com certeza ate a parte constante, o resto falta comparar com outros

--- EP3/main.py
+++ EP3/main.py
@@ -1,5 +1,6 @@
 from re import A
 import numpy as np
+import matplotlib.pyplot as plt
 import math
 import ep2
 import ep1
@@ -118,43 +119,44 @@ def funcaoescolhida(x, n, b):
             return ((x-1)*(math.e**(-x) - 1)) #u(x)
         else:
             return (math.e**x + 1) #f(x)
-    elif n == 3: ##EQUILIBRIO FORÇANTES DE CALOR 4.3
+    elif n == 3: ##EQUILIBRIO FORÇANTES DE CALOR 4.3 - COM CONSTANTES
         if b == 1: 
             return  0#u(x)
         else:
-            return  (37500000)*math.e**( (-(x-0.02/2)**2) / (0.5**2) )#f(x) 
-
+            Qmais =  37.5  * 1000000
+            Qmenos = 11.25 * 1000000
+            return  Qmais - Qmenos#f(x) 
+    elif n == 4: ##EQUILIBRIO FORÇANTES DE CALOR 4.3 - NAO CONSTANTE
+        if b == 1: 
+            return  0#u(x)
+        else:
+            Qmais = 37.5 * 1000000
+            Qmenos =  7.5 * 1000000
+            L = 0.02
+            sigma = 1
+            theta = 0.01
+            return  (Qmais)*math.e**( (-(x-(L/2))**2) / (sigma**2) ) - (Qmenos)*( math.e**((-x**2)/(theta**2)) + math.e**((-(x-L)**2)/(theta**2)) )#f(x) 
 
 ################ MAINS ###################
 
-def main_equilibriocomforcantesdecalor(n): #4.3
+def main_equilibriocomforcantesdecalor(n, plotar): #4.3
     L = 0.02
+    va = 20 + 273.5
+    vb = 20 + 273.5
     def k(x):
         return 3.6
     def q(x):
         return 0
-    funcaoselect = 3
+    #CALOR TOTAL CONSTANTE:
+    funcaoselect = 4
     h = L/(n+1)
     x = np.zeros(n+2)
     for i in range(0, n+2, 1):
         x[i] = (i)*h
-    #print("VETOR X:")
-    #print(x)
     A, a, b, c = montarMatrizA_k1_q0(n, h, x, k, q)
-    #print("MATRIZ A (As(a), Am(b), Ai(c)):")
-    #print(A)
-    #print(a)
-    #print(b)
-    #print(c)
     B = montarMatrizB(n, x, funcaoselect, h)
-    #print("MATRIZ B(d):")
-    #print(B)
-
-    ##DECOMPOSIÇÃO LU
     l, u = ep1.decompLU(a ,b ,c, n)
     alphas = ep1.solucaoLU(l, u, c, B, n)
-    #print("ALPHAS / Solucao do LU:")
-    #print(alphas)
 
     def ubarra(xvar, alphas, x, h, n):
         ubarra = 0.0
@@ -162,19 +164,60 @@ def main_equilibriocomforcantesdecalor(n): #4.3
             ubarra += alphas[j-1]*phi(xvar,x[j-1],x[j+1],x[j],h)
         return ubarra
 
-    x0 = ubarra(0, alphas, x, h, n)
-    x1 = ubarra(0.010, alphas, x, h, n)
-    x2 = ubarra(0.015, alphas, x, h, n)
-    x3 = ubarra(0.020, alphas, x, h, n)
+    resultado = np.zeros(n+2)
+    for i in range(0,n+2):
+        resultado[i] = ubarra(x[i], alphas, x, h, n) + va + ((vb-va) * x[i]) - 273.5 #ubarra(x[i], alphasGER, x, h, n) + va + (vb-va) * x[i] - ubarra(x[i], alphasDIS, x, h, n) - 273.5
 
-    print("Valor de calor em x =  0mm: " + str(x0))
-    print("Valor de calor em x = 10mm: " + str(x1))
-    print("Valor de calor em x = 15mm: " + str(x2))
-    print("Valor de calor em x = 20mm: " + str(x3))
+    # RESULTADOS:
+    if plotar == 1:
+        plt.plot(x*1000, resultado)
+        plt.title('Temperatura')
+        plt.ylabel('Temperatura (C°)')
+        plt.xlabel('x(mm)')
+        plt.show()
+    for i in range(0,n+2):
+        print("A temperaatura em x = " + str(x[i]) + "mm vale: " +  str(resultado[i]) + " °C")
 
-
-    #Calcular erro
     
+
+def main_equilibriocomforcantesdecalor_constante(n, plotar): #4.3
+    L = 0.02
+    va = 20 + 273.5
+    vb = 20 + 273.5
+    def k(x):
+        return 3.6
+    def q(x):
+        return 0
+    #CALOR TOTAL CONSTANTE:
+    funcaoselect = 3
+    h = L/(n+1)
+    x = np.zeros(n+2)
+    for i in range(0, n+2, 1):
+        x[i] = (i)*h
+    A, a, b, c = montarMatrizA_k1_q0(n, h, x, k, q)
+    B = montarMatrizB(n, x, funcaoselect, h)
+    l, u = ep1.decompLU(a ,b ,c, n)
+    alphas = ep1.solucaoLU(l, u, c, B, n)
+
+    def ubarra(xvar, alphas, x, h, n):
+        ubarra = 0.0
+        for j in range(1,n+1):
+            ubarra += alphas[j-1]*phi(xvar,x[j-1],x[j+1],x[j],h)
+        return ubarra
+
+    resultado = np.zeros(n+2)
+    for i in range(0,n+2):
+        resultado[i] = ubarra(x[i], alphas, x, h, n) + va + ((vb-va) * x[i]) - 273.5 #ubarra(x[i], alphasGER, x, h, n) + va + (vb-va) * x[i] - ubarra(x[i], alphasDIS, x, h, n) - 273.5
+
+    #RESULTADOS:
+    if plotar == 1:
+        plt.plot(x*1000, resultado)
+        plt.title('Temperatura com geração de calor constante')
+        plt.ylabel('Temperatura (C°)')
+        plt.xlabel('x(mm)')
+        plt.show()
+    for i in range(0,n+2):
+        print("A temperatura em x = " + str(x[i]) + "mm vale: " +  str(resultado[i]) + " °C")
 
 
 def main_validacao_comp(n): # 4.2 complemento
@@ -244,8 +287,10 @@ def main_validacao(n): # 4.2
     print("Erro encontrado: " + str(erro))
 
 if __name__ == "__main__":
-    n = 63
+    n = 7
+    plotar = 0
     #main_validacao(n)
     #main_validacao_comp(n)
-    main_equilibriocomforcantesdecalor(n)
+    main_equilibriocomforcantesdecalor_constante(n, plotar)
+    #main_equilibriocomforcantesdecalor(n, plotar)
     
